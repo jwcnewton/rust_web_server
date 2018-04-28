ffc01b491d2ce6b8381665da86d627d4cc4c97de
jwcnewton <jwcnewton@gmail.com>
2018-04-28 15:46:50 +0100

    Added html pages and request deserializer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::io::prelude::*;
+use std::fs::File;
 
-const RES_200:[u8; 19] = [72, 84, 84, 80, 47, 49, 46, 49, 32, 50, 48, 48, 32, 79, 75, 13, 10, 13, 10];
+pub mod reqlib;
+
+static INDEX_PAGE:&'static str = "./src/pages/index.html";
+static NOT_FOUND_PAGE:&'static str = "./src/pages/404.html";
 
 fn main() {
     server_on();
@@ -13,7 +17,6 @@ fn server_on(){
    
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established!");
         handle_connection(stream);
     }
 }
@@ -21,7 +24,35 @@ fn server_on(){
 fn handle_connection(mut stream: TcpStream){
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
+    let request_string = String::from_utf8_lossy(&buffer[..]);
+  
+    let request = reqlib::str_to_request(request_string.to_string());
+    if request.uri == "/" {
+        handle_index(stream);
+    } else {
+        handle_not_found(stream);
+    }
+}
 
-    stream.write(&RES_200).unwrap(); 
+fn handle_not_found(mut stream: TcpStream){
+    let index_page_content = get_page_content(NOT_FOUND_PAGE);
+    let res = format!("HTTP/1.1 200 Ok \r\n\r\n {}", index_page_content);
+
+    stream.write(&res.as_bytes()).unwrap(); 
     stream.flush().unwrap();
+}
+
+fn handle_index(mut stream: TcpStream){
+    let index_page_content = get_page_content(INDEX_PAGE);
+    let res = format!("HTTP/1.1 200 Ok \r\n\r\n {}", index_page_content);
+
+    stream.write(&res.as_bytes()).unwrap(); 
+    stream.flush().unwrap();
+}
+
+fn get_page_content(page_ref: &'static str)->String {
+    let mut f = File::open(page_ref).unwrap();
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).unwrap();
+    return contents;
 }
---
+++ src/reqlib.rs
@@ -0,0 +1,38 @@
+pub struct HttpReq {
+   pub uri: String,
+   pub verb: String,
+   pub res_type: String
+}
+
+pub fn str_to_request(req:String)->HttpReq {
+    let request_lf = req.lines().nth(0).unwrap().to_string();
+    let uri = get_request_line_uri(&request_lf);
+    let verb = get_request_line_verb(&request_lf);
+    let content_type = get_request_line_type(&req.to_string());
+    return HttpReq{ 
+        uri: uri,
+        verb: verb,
+        res_type: content_type
+    };
+}
+
+fn get_request_line_verb(request_string: &String)->String {
+    return request_string.split_whitespace().nth(0).unwrap().trim().to_string();
+}
+
+fn get_request_line_uri(request_string: &String)->String {
+    return request_string.split_whitespace().nth(1).unwrap().trim().to_string();
+}
+
+fn get_request_line_type(request_string: &String)->String {
+    let accept_index = request_string
+        .find("Accept:")
+        .unwrap_or(0);
+    let accept_sub: String = request_string.chars()
+        .skip(accept_index)
+        .take(request_string.len())
+        .collect();
+    let accept_line = accept_sub.lines().nth(0).unwrap().to_string();
+    let accept_val = accept_line.split_whitespace().nth(1).unwrap();
+    return accept_val.trim().to_string();
+}
